a4cb68edf3fb526b4bde5fb161928dae7077a4eb
Sally Schaffner <schaffners@vpcc.edu>
2024-10-24 10:31:46 -0400

    Corrected minor errors

--- Chapter 4 Programming Examples.cpp
+++ Chapter 4 Programming Examples.cpp
@@ -12,12 +12,12 @@ int main()
 
     string primary1, primary2;
 
-    cout << "Enter two primary colors(red, green, or blue), all lowercase: ";
+    cout << "Enter two primary colors(red, yellow, or blue), all lowercase: ";
     cin >> primary1 >> primary2;
 
     if (primary1 == primary2)
         cout << "You get the primary color: " << primary1 << endl;
-    else if ((primary1 == RED && primary2 == BLUE) || (primary1 == BLUE && primary1 == RED))
+    else if ((primary1 == RED && primary2 == BLUE) || (primary1 == BLUE && primary2 == RED))
         cout << "The secondary color is purple" << endl;
     else if ((primary1 == RED and primary2 == YELLOW) || (primary1 == YELLOW && primary2 == RED))
         cout << "The secondary color is orange" << endl;
